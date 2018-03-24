9d4fb6d7be48ebab118d0355e0031132e2c277f3
keesL <kees@leune.org>
2018-03-23 20:58:15 -0400

    Initial push

---
+++ config.py
@@ -0,0 +1,14 @@
+# Twitter2Coolsign configuration
+
+class Config:
+    # accounts to monitor
+    twits=['adelphimathcs', 'adelphiACM', 'leune']
+
+    # max number of tweets to retrieve
+    numTweets=10
+
+    # API credentials; get these from Twitter and plug them in
+    twitter_consumerKey=''
+    twitter_consumerSecret=''
+    twitter_access_token=''
+    twitter_access_token_secret=''
